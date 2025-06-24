594bd5b1589b52b7f80bfffed1f9f2fcda27fec5
Alexander Yuryshev <yuryshevalexander@gmail.com>
2025-06-24 21:16:05 +0700

    Init

---
+++ src/server/auth/config.ts
@@ -0,0 +1,33 @@
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import type { DefaultSession, NextAuthConfig } from "next-auth";
+import DiscordProvider from "next-auth/providers/discord";
+import { db } from "@/server/db";
+
+declare module "next-auth" {
+  interface Session extends DefaultSession {
+    user: {
+      id: string;
+    } & DefaultSession["user"];
+  }
+}
+
+export const authConfig = {
+  providers: [
+    DiscordProvider({
+      clientId: process.env.DISCORD_CLIENT_ID!,
+      clientSecret: process.env.DISCORD_CLIENT_SECRET!,
+    }),
+  ],
+  adapter: PrismaAdapter(db),
+  callbacks: {
+    session: ({ session, user }) => ({
+      ...session,
+      user: {
+        ...session.user,
+        id: user.id,
+      },
+    }),
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+  trustHost: true,
+} satisfies NextAuthConfig;
